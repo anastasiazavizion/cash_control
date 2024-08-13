72ce029251929cdc48ab81c59d3132b936daba5b
Anastasia Zavizion <a.zavizion.a@gmail.com>
2024-08-13 15:48:29 +0200

    User roles and permissions

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enum\Role;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->delete();
+
+        if(!User::role(Role::ADMIN->value)->exists()){
+            User::factory()->admin()->create();
+        }
+
+        User::factory(3)->moderator()->create();
+
+        $product = Product::first();
+
+        User::factory(500)->create()->each(function ($user) use ($product){
+            $user->wishes()->attach($product->id);
+        });
+
+    }
+}
