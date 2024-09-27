8cd8246770977b118808e63eab8796cda971c003
Anastasia Zavizion <a.zavizion.a@gmail.com>
2024-09-27 11:14:51 +0200

    Sass improves

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,41 +1,40 @@
 <script setup>
 import {useStore} from "vuex";
 import {computed, onMounted, ref} from "vue";
-import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
+import {TabGroup, TabList, Tab, TabPanels, TabPanel} from '@headlessui/vue'
 
 import listenLimitEvents from '../hooks/listenLimitEvents.js'
 
 import PaymentsByCategoryChart from "./Payment/PaymentsByCategoryChart.vue";
 import PaymentsByCategory from "./Payment/PaymentsByCategory.vue";
 import AddNewPaymentDialog from "./Payment/AddNewPaymentDialog.vue";
-import {PlusIcon, CurrencyDollarIcon}  from '@heroicons/vue/24/solid'
+import {PlusIcon, CurrencyDollarIcon} from '@heroicons/vue/24/solid'
 
 const store = useStore();
 
-const categories = computed(()=>{
-    return  store.getters['category/categories'];
+const categories = computed(() => {
+    return store.getters['category/categories'];
 })
 
-const paymentTypes = computed(()=>{
-    return store.getters['paymentType/paymentTypes'].map((item, index)=>{
+const paymentTypes = computed(() => {
+    return store.getters['paymentType/paymentTypes'].map((item, index) => {
         return {
             ...item,
-            is_active : index === 0
+            is_active: index === 0
         }
     });
 })
 
 
-
-const total = computed(()=>{
+const total = computed(() => {
     return store.getters['payment/total'];
 })
 
-const totalSum = computed(()=>{
+const totalSum = computed(() => {
     return store.getters['payment/totalSum'];
 })
 
-const paymentsByCategory = computed(()=>{
+const paymentsByCategory = computed(() => {
     return store.getters['payment/paymentsByCategory'];
 })
 
@@ -69,21 +68,21 @@ onMounted(async () => {
 })
 
 const paymentForm = ref({
-    amount:0,
-    category_id:null,
-    payment_date:null,
-    description:null,
-    payment_type_id:1,
-    payment_currency_id:1,
+    amount: 0,
+    category_id: null,
+    payment_date: null,
+    description: null,
+    payment_type_id: 1,
+    payment_currency_id: 1,
 })
 
 const openDialog = ref(false);
 
-function closeDialog(){
+function closeDialog() {
     openDialog.value = false;
 }
 
-const errors = computed(()=>{
+const errors = computed(() => {
     return store.getters['payment/errors'];
 })
 
@@ -91,7 +90,7 @@ const errors = computed(()=>{
 async function savePayment(form) {
     await store.dispatch('payment/savePayment', form);
 
-    if(Object.keys(errors.value).length === 0){
+    if (Object.keys(errors.value).length === 0) {
         await getPaymentsByTypeId(form.payment_type_id);
         closeDialog();
     }
@@ -103,7 +102,7 @@ async function setActivePaymentType(id) {
     await getPaymentsByTypeId(id);
 }
 
-onMounted(()=>{
+onMounted(() => {
     listenLimitEvents();
 })
 
@@ -116,19 +115,19 @@ async function removePayment(id) {
 
 const visibleCategories = ref([]);
 
-function saveVisibleCategories(categories){
+function saveVisibleCategories(categories) {
     visibleCategories.value = categories;
 }
 
-function makeReport(type){
+function makeReport(type) {
     axios.get('/report', {
-        params:{type:type},
+        params: {type: type},
         responseType: 'blob'
     }).then(response => {
         const url = window.URL.createObjectURL(new Blob([response.data]));
         const link = document.createElement('a');
         link.href = url;
-        link.setAttribute('download', 'report.'+type);
+        link.setAttribute('download', 'report.' + type);
         document.body.appendChild(link);
         link.click();
         link.remove(); // Clean up the DOM after download
@@ -137,71 +136,81 @@ function makeReport(type){
     });
 }
 
-import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
+import {Menu, MenuButton, MenuItems, MenuItem} from '@headlessui/vue'
 import Price from "../Components/Price.vue";
 import PrimaryButton from "../Components/PrimaryButton.vue";
+
 const reportLinks = [
-    { label: 'Pdf', type:'pdf'},
-    { label: 'Excel', type:'xlsx' },
+    {label: 'Pdf', type: 'pdf'},
+    {label: 'Excel', type: 'xlsx'},
 ]
+
 
 </script>
 
 <template>
     <div>
-    <AddNewPaymentDialog :errors="errors" @close-dialog="closeDialog" @save-payment="savePayment" :payment-form="paymentForm"  :open-dialog="openDialog"/>
+        <AddNewPaymentDialog :errors="errors" @close-dialog="closeDialog" @save-payment="savePayment"
+                             :payment-form="paymentForm" :open-dialog="openDialog"/>
 
-    <div class="relative mb-4">
-        <Menu>
-            <MenuButton class="primary-btn">Create report</MenuButton>
-            <MenuItems class="absolute left-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
-                <MenuItem
-                    as="template"
-                    @click="makeReport(link.type)"
-                    v-for="link in reportLinks"
-                    :key="link.type"
-                    class="cursor-pointer hover:bg-slate-100 ui-active:bg-blue-500 ui-active:text-white ui-not-active:bg-white ui-not-active:text-black  text-gray-900 group flex w-full items-center  px-2 py-2 text-sm"
-                >
-                    <div>{{link.label }}</div>
-                </MenuItem>
+        <div class="fixed">
+            <Menu as="div" class="relative inline-block text-left">
+                <MenuButton class="primary-btn">Create report</MenuButton>
+                <MenuItems
+                    class="absolute left-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+                    <MenuItem
+                        as="template"
+                        @click="makeReport(link.type)"
+                        v-for="link in reportLinks"
+                        :key="link.type"
+                        class="cursor-pointer hover:bg-slate-100 ui-active:bg-blue-500 ui-active:text-white ui-not-active:bg-white ui-not-active:text-black  text-gray-900 group flex w-full items-center  px-2 py-2 text-sm"
+                    >
+                        <div>{{ link.label }}</div>
+                    </MenuItem>
 
-            </MenuItems>
-        </Menu>
-    </div>
+                </MenuItems>
+            </Menu>
+        </div>
 
-    <div class="mb-4 text-center">
-        <CurrencyDollarIcon class="h-6"></CurrencyDollarIcon> Total <Price>{{totalSum}}</Price>
-    </div>
 
-    <TabGroup>
-        <TabList class="text-center grid grid-cols-2">
-            <Tab  :class="[activePaymentType === (id+1) ? 'active-tab' : '']" class="mr-4 tab" @click="setActivePaymentType(paymentType.id)" :key="paymentType.id" v-for="(paymentType,id) in paymentTypes">
-                {{paymentType.name}}
-            </Tab>
-        </TabList>
+        <div class="mb-4 mt-4 text-center">
+            <CurrencyDollarIcon class="h-6"></CurrencyDollarIcon>
+            Total
+            <Price>{{ totalSum }}</Price>
+        </div>
 
-        <TabPanels class="mt-4 min-h-40 sm:min-h-80">
-            <div>
-               <TabPanel :key="paymentType.id" v-for="paymentType in paymentTypes">
+        <TabGroup>
+            <TabList class="text-center grid grid-cols-2 mt-8">
+                <Tab :class="[activePaymentType === (id+1) ? 'active-tab' : '']" class="mr-4 tab"
+                     @click="setActivePaymentType(paymentType.id)" :key="paymentType.id"
+                     v-for="(paymentType,id) in paymentTypes">
+                    {{ paymentType.name }}
+                </Tab>
+            </TabList>
 
-                   <PrimaryButton class="mb-4" @click="openDialog = true">
-                      Add new transaction
-                   </PrimaryButton>
+            <TabPanels class="mt-4 min-h-40 sm:min-h-80">
+                <div>
+                    <TabPanel :key="paymentType.id" v-for="paymentType in paymentTypes">
 
-                   <div>
-                       <PaymentsByCategoryChart :total="total" :paymentsByCategory="paymentsByCategory" v-if="loaded"></PaymentsByCategoryChart>
-                   </div>
-                   <div class="mt-4">
-                       <PaymentsByCategory @save-visible-categories="saveVisibleCategories"
-                                           @remove-payment="removePayment" v-if="loaded"
-                                           :categories="paymentsByCategory"
-                                           :visibleCategories="visibleCategories"
-                       >
-                       </PaymentsByCategory>
-                   </div>
-                </TabPanel>
-            </div>
-        </TabPanels>
-    </TabGroup>
+                        <PrimaryButton class="mb-4" @click="openDialog = true">
+                            Add new transaction
+                        </PrimaryButton>
+
+                        <div>
+                            <PaymentsByCategoryChart :total="total" :paymentsByCategory="paymentsByCategory"
+                                                     v-if="loaded"></PaymentsByCategoryChart>
+                        </div>
+                        <div class="mt-4">
+                            <PaymentsByCategory @save-visible-categories="saveVisibleCategories"
+                                                @remove-payment="removePayment" v-if="loaded"
+                                                :categories="paymentsByCategory"
+                                                :visibleCategories="visibleCategories"
+                            >
+                            </PaymentsByCategory>
+                        </div>
+                    </TabPanel>
+                </div>
+            </TabPanels>
+        </TabGroup>
     </div>
 </template>
