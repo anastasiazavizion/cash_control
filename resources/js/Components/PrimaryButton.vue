3d7e3eecc0e32ac8b06e9b2714f6e14e1834302f
Anastasia Zavizion <a.zavizion.a@gmail.com>
2024-10-01 12:04:28 +0200

    Fixed button style

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        class="inline-flex items-center px-4 py-2 primary-btn border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+        class="primary-btn"
     >
         <slot />
     </button>
