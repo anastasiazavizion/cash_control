927234e8844d22ac8df194a72437fecec64062de
Anastasia Zavizion <a.zavizion.a@gmail.com>
2024-10-07 17:50:44 +0200

    Fixed footer

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -74,79 +74,84 @@ const showRate = ref(false);
 </script>
 
 <template>
-    <nav>
-    <Dialog :open="showRate" @close="showRate = false" class="relative z-50 dialog">
-        <div class="fixed inset-0 bg-black/30" aria-hidden="true" />
-        <div class="fixed inset-0 flex w-screen items-center justify-center p-4">
-            <DialogPanel class="w-full max-w-xl rounded bg-white dialog-panel">
-                <ExchangeRate></ExchangeRate>
-            </DialogPanel>
-        </div>
-    </Dialog>
-    <Disclosure as="nav" v-slot="{ open }">
-        <div class="mx-auto max-w-7xl menu-padding">
-            <div class="relative flex h-16 items-center justify-between">
-                <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                    <!-- Mobile menu button-->
-                    <DisclosureButton class="relative inline-flex items-center justify-center rounded-md p-2 text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                        <span class="absolute -inset-0.5" />
-                        <span class="sr-only">{{$t('Open main menu')}}</span>
-                        <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
-                        <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
-                    </DisclosureButton>
+    <div class="min-h-screen flex flex-col">
+        <nav>
+            <Dialog :open="showRate" @close="showRate = false" class="relative z-50 dialog">
+                <div class="fixed inset-0 bg-black/30" aria-hidden="true" />
+                <div class="fixed inset-0 flex w-screen items-center justify-center p-4">
+                    <DialogPanel class="w-full max-w-xl rounded bg-white dialog-panel">
+                        <ExchangeRate></ExchangeRate>
+                    </DialogPanel>
                 </div>
+            </Dialog>
+            <Disclosure as="nav" v-slot="{ open }">
+                <div class="mx-auto max-w-7xl menu-padding">
+                    <div class="relative flex h-16 items-center justify-between">
+                        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                            <!-- Mobile menu button-->
+                            <DisclosureButton class="relative inline-flex items-center justify-center rounded-md p-2 text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                <span class="absolute -inset-0.5" />
+                                <span class="sr-only">{{$t('Open main menu')}}</span>
+                                <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+                                <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+                            </DisclosureButton>
+                        </div>
 
-                <div class="flex items-center justify-center sm:items-stretch sm:justify-start mr-8">
+                        <div class="flex items-center justify-center sm:items-stretch sm:justify-start mr-8">
 
-                 <div class="flex flex-shrink-0 items-center pl-12 sm:pl-0">
-                        <img v-if="logoSrc" class="h-8 w-auto cursor-pointer" :src="logoSrc" alt="Your Company" />
-                    </div>
-                    <div class="hidden sm:ml-6 sm:block">
-                        <div class="flex space-x-4">
-                            <router-link
-                                v-for="item in visibleNavigation"
-                                :key="item.name"
-                            :to="item.href"
-                            class="rounded-md px-3 py-2 text-sm font-medium  hover:text-white"
-                            @click="navigate(item.href)"
-                            >
-                            {{ item.name }}
-                            </router-link>
+                            <div class="flex flex-shrink-0 items-center pl-12 sm:pl-0">
+                                <img v-if="logoSrc" class="h-8 w-auto cursor-pointer" :src="logoSrc" alt="Your Company" />
+                            </div>
+                            <div class="hidden sm:ml-6 sm:block">
+                                <div class="flex space-x-4">
+                                    <router-link
+                                        v-for="item in visibleNavigation"
+                                        :key="item.name"
+                                        :to="item.href"
+                                        class="rounded-md px-3 py-2 text-sm font-medium  hover:text-white"
+                                        @click="navigate(item.href)"
+                                    >
+                                        {{ item.name }}
+                                    </router-link>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="absolute inset-y-0 right-0 flex gap-4 items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                            <ExchangeRate class="hidden sm:inline text-white"></ExchangeRate>
+                            <InformationCircleIcon @click="showRate = true" class="sm:hidden text-white h-8 cursor-pointer"></InformationCircleIcon>
+                            <Menu v-if="authenticated" as="div" class="relative">
+                                <div class="flex gap-4 items-center text-white \">
+                                    <span class="">{{user.name}}</span>
+                                    <MenuButton  class="text-white relative items-center gap-4  flex rounded-full  text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                        <span class="absolute -inset-1.5" />
+                                        <img v-if="user.avatar" class="h-8 w-8 rounded-full" :src="user.avatar" alt="" />
+                                        <UserIcon class="h-6 w-6 rounded-full" v-else/>
+                                    </MenuButton>
+                                </div>
+                                <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                    <MenuItem>
+                                        <router-link :class="['block px-4 py-2 text-sm text-gray-700']"  class="rounded-md px-3 py-2 text-sm font-medium" :to="{name:'settings'}">{{$t('Settings')}}</router-link>
+                                    </MenuItem>
+                                </MenuItems>
+                            </Menu>
+                            <Locale></Locale>
                         </div>
                     </div>
                 </div>
+                <DisclosurePanel class="sm:hidden menu-padding">
+                    <div class="flex flex-col gap-2">
+                        <DisclosureButton v-for="item in visibleNavigation" :key="item.name" as="a" :href="item.href">{{ item.name }}</DisclosureButton>
+                    </div>
+                </DisclosurePanel>
+            </Disclosure>
+        </nav>
 
-                <div class="absolute inset-y-0 right-0 flex gap-4 items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                    <ExchangeRate class="hidden sm:inline text-white"></ExchangeRate>
-                    <InformationCircleIcon @click="showRate = true" class="sm:hidden text-white h-8 cursor-pointer"></InformationCircleIcon>
-                    <Menu v-if="authenticated" as="div" class="relative">
-                        <div class="flex gap-4 items-center text-white \">
-                            <span class="">{{user.name}}</span>
-                            <MenuButton  class="text-white relative items-center gap-4  flex rounded-full  text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                                <span class="absolute -inset-1.5" />
-                                <img v-if="user.avatar" class="h-8 w-8 rounded-full" :src="user.avatar" alt="" />
-                                <UserIcon class="h-6 w-6 rounded-full" v-else/>
-                            </MenuButton>
-                        </div>
-                        <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            <MenuItem>
-                                <router-link :class="['block px-4 py-2 text-sm text-gray-700']"  class="rounded-md px-3 py-2 text-sm font-medium" :to="{name:'settings'}">{{$t('Settings')}}</router-link>
-                            </MenuItem>
-                        </MenuItems>
-                    </Menu>
-                    <Locale></Locale>
-                </div>
-            </div>
+        <div class="container default-component flex-grow">
+            <main>
+                <router-view></router-view>
+            </main>
         </div>
-        <DisclosurePanel class="sm:hidden menu-padding">
-            <div class="flex flex-col gap-2">
-                <DisclosureButton v-for="item in visibleNavigation" :key="item.name" as="a" :href="item.href">{{ item.name }}</DisclosureButton>
-            </div>
-        </DisclosurePanel>
-    </Disclosure>
-    </nav>
-    <div class="container mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 mt-4">
-        <router-view></router-view>
+        <Footer/>
     </div>
-    <Footer/>
 </template>
